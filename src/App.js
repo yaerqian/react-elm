95fb4ab585cb4d21d156df014cb908434d5b74e0
noOne <1332859786@qq.com>
2019-06-23 20:40:04 +0800

    修改rem 代码

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style/common.scss'
 import Header from './common/header/index'
-
+import './static/iconfont/iconfont.css'
 function App() {
   return (
     <div className="App">
-        <Header />
+        <Header name="search" />
         {/* learn react */}
     </div>
   );
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import './index.scss'
 
 class Header extends Component {
+
+    // constructor(props) {
+    //     super(props)
+    // };
+
     render() {
+        const { name } = this.props;
         return (
-            <header id="head_top">
-                header
-            </header>
+                <header id="head_top">
+                    <i class="iconfont head_logo">&#59170;</i>
+                    {name === "login" && <h1>login</h1>}
+                    {/* {name === "search" && <div>search</div>} */}
+                </header> 
         )
     }
 }
