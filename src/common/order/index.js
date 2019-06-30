c5731ca14f8c0d04042354a32c5f519c301e8014
noOne <1332859786@qq.com>
2019-06-30 20:04:58 +0800

    订单页面未登录效果, 修改 mixin scss

--- src/common/order/index.js
+++ src/common/order/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-
+import img from '../../static/image/download.png'
+import './index.scss'
 class Order extends Component {
 
     render() {
         return(
-            <div>订单</div>
+            <div>
+                <div className="order-not-login">
+                    <img src={img} alt=""/>
+                    <h3>登录后查看外卖订单</h3>
+                    <div>
+                        立即登录
+                    </div>
+                </div>
+            </div>
         )
     }
 
